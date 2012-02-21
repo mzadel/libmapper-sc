8f2dba6f0d2f0a0427467529e464a253a8e98a02
Mark Zadel <zadel@music.mcgill.ca>
2012-02-21 14:12:49 -0500

    clear my changes out of PyrPrimitive.cpp

--- common/Source/lang/LangPrimSource/PyrPrimitive.cpp
+++ common/Source/lang/LangPrimSource/PyrPrimitive.cpp
@@ -45,8 +45,6 @@
 #include "PyrDeepFreezer.h"
 //#include "Wacom.h"
 #include "InitAlloc.h"
-#include <mapper/mapper.h>
-#include <lo/lo.h>
 
 #define SANITYCHECK 0
 int yyparse();
@@ -3376,71 +3374,6 @@ int prBlork(struct VMGlobals *g, int numArgsPushed)
 }
 
 
-mapper_device my_device = NULL;
-
-int mapperJustACall(struct VMGlobals *g, int numArgsPushed);
-int mapperJustACall(struct VMGlobals *g, int numArgsPushed)
-{
-	return errNone;
-}
-
-
-int mapperDevNew(struct VMGlobals *g, int numArgsPushed);
-int mapperDevNew(struct VMGlobals *g, int numArgsPushed)
-{
-	my_device = mdev_new("supercollider", 9444, 0);
-	return errNone;
-}
-
-float min0 = 0;
-float max1000 = 1000;
-
-float currentvalue = 0.0;
-
-void handler_freq(mapper_signal sig, mapper_db_signal props, mapper_timetag_t *timetag, void *pfreq)
-{
-	// store the value in a local variable to poll later
-	currentvalue = *(float*)pfreq;
-}
-
-int mapperAddInput(struct VMGlobals *g, int numArgsPushed);
-int mapperAddInput(struct VMGlobals *g, int numArgsPushed)
-{
-    mdev_add_input(my_device, "/freq", 1, 'f', 0, &min0, &max1000, handler_freq, NULL);
-	return errNone;
-}
-
-int mapperPoll(struct VMGlobals *g, int numArgsPushed);
-int mapperPoll(struct VMGlobals *g, int numArgsPushed)
-{
-	int numhandled = 1;
-	while ( numhandled > 0 ) {
-		numhandled = mdev_poll(my_device, 0);
-	}
-	return errNone;
-}
-
-int mapperDevFree(struct VMGlobals *g, int numArgsPushed);
-int mapperDevFree(struct VMGlobals *g, int numArgsPushed)
-{
-	mdev_free( my_device );
-	return errNone;
-}
-
-int mapperGetCurrentValue(struct VMGlobals *g, int numArgsPushed);
-int mapperGetCurrentValue(struct VMGlobals *g, int numArgsPushed)
-{
-	// pull the last polled value out of currentvalue
-
-	PyrSlot *receiver = g->sp;
-
-	// set the receiver slot to return the value
-	SetFloat(receiver, currentvalue);
-
-	return errNone;
-}
-
-
 #define PRIMGROWSIZE 480
 PrimitiveTable gPrimitiveTable;
 
@@ -3993,14 +3926,6 @@ void initPrimitives()
 	definePrimitive(base, index++, "_Blork", prBlork, 1, 0);
 	definePrimitive(base, index++, "_UGenCodeString", prUGenCodeString, 5, 0);
 
-	// libmapper
-	definePrimitive(base, index++, "_MapperJustACall", mapperJustACall, 1, 0);
-	definePrimitive(base, index++, "_MapperDevNew", mapperDevNew, 1, 0);
-	definePrimitive(base, index++, "_MapperAddInput", mapperAddInput, 1, 0);
-	definePrimitive(base, index++, "_MapperPoll", mapperPoll, 1, 0);
-	definePrimitive(base, index++, "_MapperDevFree", mapperDevFree, 1, 0);
-	definePrimitive(base, index++, "_MapperGetCurrentValue", mapperGetCurrentValue, 1, 0);
-
 	//void initOscilPrimitives();
 	//void initControllerPrimitives();
 
@@ -4111,4 +4036,3 @@ void initThreads()
 }
 
 
-// vim:sw=4:ts=4:
