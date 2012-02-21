fc245841956eab4db0b467dc443883c4157f893f
Mark Zadel <zadel@music.mcgill.ca>
2012-02-21 14:03:44 -0500

    added a file for holding the libmapper primitives

---
+++ common/Source/lang/LangPrimSource/PyrMapperPrim.cpp
@@ -0,0 +1,19 @@
+
+#include "PyrPrimitive.h"
+#include "VMGlobals.h"   // is this needed?
+
+void initMapperPrimitives()
+{
+	int base, index = 0;
+
+	base = nextPrimitiveIndex();
+
+	// libmapper
+	definePrimitive(base, index++, "_MapperJustACall", mapperJustACall, 1, 0);
+	definePrimitive(base, index++, "_MapperDevNew", mapperDevNew, 1, 0);
+	definePrimitive(base, index++, "_MapperAddInput", mapperAddInput, 1, 0);
+	definePrimitive(base, index++, "_MapperPoll", mapperPoll, 1, 0);
+	definePrimitive(base, index++, "_MapperDevFree", mapperDevFree, 1, 0);
+	definePrimitive(base, index++, "_MapperGetCurrentValue", mapperGetCurrentValue, 1, 0);
+}
+
