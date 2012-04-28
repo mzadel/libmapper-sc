e1dbe8f6480cf2a4fa4ce78e9fec13fb9b7a9de2
Mark Zadel <zadel@music.mcgill.ca>
2012-04-28 17:37:06 -0400

    filled in the mapperDeviceNew() and mapperDeviceFree() stubs
    added dataptr in the Mapper class on the sclang side to hold a pointer
    to the libmapper device.
    
    also need to link against the mapper C library

--- common/Source/lang/LangPrimSource/PyrMapperPrim.cpp
+++ common/Source/lang/LangPrimSource/PyrMapperPrim.cpp
@@ -1,15 +1,43 @@
 
+#include "PyrObject.h"
 #include "PyrPrimitive.h"
+#include "VMGlobals.h"
+
+#include <mapper/mapper.h>
 
 int mapperDeviceNew(struct VMGlobals *g, int numArgsPushed);
 int mapperDeviceNew(struct VMGlobals *g, int numArgsPushed)
 {
+	PyrSlot *a = g->sp - 2;
+	PyrSlot *b = g->sp - 1;
+	PyrSlot *c = g->sp;
+
+	int err, portrequested;
+	PyrSymbol *devicename;
+	mapper_device dev;
+
+	err = slotSymbolVal(b, &devicename);
+	if (err) return errWrongType;
+
+	err = slotIntVal(c, &portrequested);
+	if (err) return errWrongType;
+
+	dev = mdev_new(devicename->name, portrequested, 0);
+
+	SetPtr(slotRawObject(a)->slots+0, dev);
+
 	return errNone;
 }
 
 int mapperDeviceFree(struct VMGlobals *g, int numArgsPushed);
 int mapperDeviceFree(struct VMGlobals *g, int numArgsPushed)
 {
+	PyrSlot *a = g->sp;
+
+	mapper_device dev = slotRawPtr(slotRawObject(a)->slots+0);
+
+	mdev_free( dev );
+
 	return errNone;
 }
 
@@ -25,7 +53,7 @@ void initMapperPrimitives()
 
 	base = nextPrimitiveIndex();
 
-	definePrimitive(base, index++, "_MapperDeviceNew", mapperDeviceNew, 1, 0);
+	definePrimitive(base, index++, "_MapperDeviceNew", mapperDeviceNew, 3, 0);
 	definePrimitive(base, index++, "_MapperDeviceFree", mapperDeviceFree, 1, 0);
 	definePrimitive(base, index++, "_MapperDeviceAddInput", mapperDeviceAddInput, 1, 0);
 
